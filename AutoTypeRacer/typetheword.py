314112bc0b94150d95cd8d319e2beea19537287f
Christos Avramis <it174918@it.teithe.gr>
2020-04-15 16:31:34 +0300

    Update typetheword.py
    cheesy update to the way virtual keyboard works

--- AutoTypeRacer/typetheword.py
+++ AutoTypeRacer/typetheword.py
@@ -18,33 +18,41 @@ class TypeRacerBot():
     def loadPage(self,url):
         #load the page
         self.browser.get(url)
+        print("loading page")
 
         #skip accept terms
         element = WebDriverWait(self.browser, 10).until(
         EC.element_to_be_clickable((By.XPATH, '//*[@id="qcCmpButtons"]/button[2]')))
         element.click()
+        print("skipped accept terms")
         
     def play_practice(self,url):
         self.loadPage(url)
         #press practice
         element = WebDriverWait(self.browser, 10).until(
         EC.element_to_be_clickable((By.XPATH, '//*[@id="dUI"]/table/tbody/tr[2]/td[2]/div/div[1]/div/table/tbody/tr[3]/td/table/tbody/tr/td[2]/table/tbody/tr[1]/td/a')))
+        time.sleep(1)
         element.click()
+        print("selected practice")
 
         #wait for the timer and get the text
         path = '//*[@id="gwt-uid-15"]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr[1]/td/div'
         element = WebDriverWait(self.browser, 10).until(
         EC.presence_of_element_located((By.XPATH, path)))
         contect = element.get_attribute('innerHTML')
+        print("waited for the text to be readable")
 
-        print("waiting for the light")
         #we must wait for the light
         path = '/html/body/div[6]/div/div/div'
         element = WebDriverWait(self.browser, 10).until(
         EC.presence_of_element_located((By.XPATH, path)))
+        print("waited for the signs to pop up")
 
+
+        print("running the virtual keyboard")
         #run the virtual keyboard
         self._virtualKeyboard(contect)
+        
 
     def play_online(self, url):
         self.loadPage(url)
@@ -69,37 +77,63 @@ class TypeRacerBot():
         #run the virtual keyboard
         self._virtualKeyboard(contect)
 
+    def play_practice_unlimited(self,url):
+        self.loadPage(url)
+        #press practice
+        element = WebDriverWait(self.browser, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="dUI"]/table/tbody/tr[2]/td[2]/div/div[1]/div/table/tbody/tr[3]/td/table/tbody/tr/td[2]/table/tbody/tr[1]/td/a')))
+        time.sleep(1)
+        element.click()
+        print("selected practice")
+
+        #wait for the timer and get the text
+        path = '//*[@id="gwt-uid-15"]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr[1]/td/div'
+        element = WebDriverWait(self.browser, 10).until(
+        EC.presence_of_element_located((By.XPATH, path)))
+        contect = element.get_attribute('innerHTML')
+        print("waited for the text to be readable")
+        
+        while True:
+            
+            #we must wait for the light
+            path = '/html/body/div[6]/div/div/div'
+            element = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.XPATH, path)))
+            print("waited for the signs to pop up")
+
+
+            print("running the virtual keyboard")
+            #run the virtual keyboard
+            self._virtualKeyboard(contect)
+
+            # print("trying again")
+            # path = '//*[@id="gwt-uid-15"]/table/tbody/tr[4]/td/div/table/tbody/tr[2]/td/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr[1]/td[2]/table/tbody/tr/td[2]/table/tbody/tr/td[2]/a'
+            # element = WebDriverWait(self.browser, 10).until(
+            # EC.presence_of_element_located((By.XPATH, path)))
+            # contect = element.click()
+
+
 
     def _virtualKeyboard(self,contect):
         #remove the tags and any character between them
-        contect = re.sub('<[^>]+>', '', contect)
-
-        #we cant get the input element but luckily for us we start typing
-        delay = 100
-        errorChance = 0
-        errorThreshold = 80
-        for char in contect:
-            time.sleep(delay/1000)
-            #perform an error if we reach the threshold
-            if(errorChance >= errorThreshold):
-                errorChance = 0
-                actions = ActionChains(self.browser)
-                actions.send_keys(random.choice(string.ascii_letters))
-                actions.perform()
-                time.sleep(delay/1400)
-                actions = ActionChains(self.browser)
-                actions.send_keys(Keys.BACKSPACE)
-                actions.perform()
-            else:
-                errorChance += random.randint(1,10)
-
+        contect = re.sub('<[^>]+>', '', contect)    
+        delay= 0.01
+        for i in range(0,len(contect)-1):
+            time.sleep(delay)
             actions = ActionChains(self.browser)
-            actions.send_keys(char)
+            actions.send_keys(contect[i])
             actions.perform()
-            delay = random.randint(100,150)
-            print(char)
 
-        
+        wpm = int(self.browser.find_element_by_class_name("rankPanelWpm").get_attribute('innerHTML').split('wpm')[0])
+        while wpm >95:
+            # from the browser find the first element which as the class 'rank etc' get the innerHtml and transform the text to get the number
+            wpm = int(self.browser.find_element_by_class_name("rankPanelWpm").get_attribute('innerHTML').split('wpm')[0])
+        actions = ActionChains(self.browser)
+        actions.send_keys(contect[len(contect)-1])
+        actions.perform()
+
+
+
     
     def closeBrowser(self):
         self.browser.close()
@@ -111,5 +145,6 @@ class TypeRacerBot():
 
 typeRacerBot = TypeRacerBot()
 typeRacerBot.play_practice('https://play.typeracer.com')
-#typeRacerBot.play_online('https://play.typeracer.com/?rt=c2y7hyxk')
+#typeRacerBot.play_practice_unlimited('https://play.typeracer.com')
+#typeRacerBot.play_online('https://play.typeracer.com?rt=29avdgkppd')
 
